8a9441c35e1a4aa37afbc55acd86fa5975d58edc
ruben <rubenlagoart@gmail.com>
2021-07-21 21:31:09 +0200

    slider ok

--- jsArchivos/carrusel.js
+++ jsArchivos/carrusel.js
@@ -1,18 +1,24 @@
-let imagen = document.querySelector('div.carruselFrases img');
-let botones = document.querySelectorAll('button')
-botones.forEach(boton => boton.addEventListener('click', moverFrases));
-let imagenActual = 1;
+/* cargar contenido imagenes cuando cargue la página */
+window.addEventListener('load', function () {
 
-function moverFrases(event) {
-    imagenActual = (event.target.innerText === "+") ? imagenActual + 1 : imagenActual - 1;
-    if (imagenActual <= 0) {
-        imagenActual = 3;
-    } else if (imagenActual > 5) {
-        imagenActual = 1;
+    /* console.log("el contenido a cargado") */
+    const frases = [];
+    frases[0] = 'img/frase_1.png';
+    frases[1] = 'img/frase_2.png';
+    frases[2] = 'img/frase_3.png';
+    frases[3] = 'img/frase_4.png';
+    frases[4] = 'img/frase_5.png';
+
+    var i = 0;
+
+    function rotarFrases() {
+        document.slider.src = frases[i];
+        if (i < 4) {
+            i++
+        } else {
+            i = 0
+        }
     }
-    cambiarImagen(imagenActual);
-}
+    setInterval(rotarFrases, 3000)
 
-function cambiarImagen(pImagen) {
-    imagen.src = `img/frase_${pImagen}.png`;
-}
+})
